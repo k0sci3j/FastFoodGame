454446027b274ca736400dc3c028504c938a1a36
k0sci3j <kosciej@gmail.com>
2024-12-27 14:49:05 +0100

    adjusted speed and added all mobs

--- Scripts/StateMachine/Mobs/MoveState.cs
+++ Scripts/StateMachine/Mobs/MoveState.cs
@@ -5,7 +5,7 @@ public partial class MoveState : State
     public override void PhysicsUpdate(float delta)
     {
         Vector2 velocity = new Vector2(1,0);
-        ((Mob)RootNode).Translate(velocity * ((((Mob)RootNode).Speed)+(GameVariables.Instance.CurrentLevel*2))/10);
+        ((Mob)RootNode).Translate(delta * velocity * ((((Mob)RootNode).Speed)+(GameVariables.Instance.CurrentLevel*4)));
     }
     private void OnEatZoneBodyEntered(CharacterBody2D body)
     {
